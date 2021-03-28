6147473cd7870f46c47510e60e3665b048cc6463
Word Under Pressure DuyThai <wordunderpressureduythai@Words-MacBook-Pro.local>
2021-03-29 00:18:26 +0700

    login

---
+++ src/controllers/PassportController/local.js
@@ -0,0 +1,45 @@
+const passport = require("passport")
+const passportLocal = require("passport-local")
+const userModel = require("./../../models/userModel")
+const { transPassport } = require("./../../../lang/vi")
+
+let LocalStratery = passportLocal.Strategy
+
+let initPassportLocal = () => {
+    passport.use(new LocalStratery({
+        usernameField: "email",
+        passwordField: "password",
+        passReqToCallback: true
+    }, async(req, email, password, done) => {
+        try {
+            let user = await userModel.findByEmail(email)
+            if (!user) {
+                return done(null, false, req.flash("errors", transPassport.login_failed))
+            }
+            if (!user.local.isActive) {
+                return done(null, false, req.flash("errors", transPassport.account_not_active))
+            }
+            let checkPassword = await user.comparePassword(password)
+            if (!checkPassword) {
+                return done(null, false, req.flash("errors", transPassport.login_failed))
+            }
+            return done(null, user, req.flash("success", transPassport.login_success(user.username)))
+        } catch (error) {
+            console.log(error)
+            return done(null, false, req.flash("errors", transPassport.server_error))
+        }
+    }))
+    passport.serializeUser((user, done) => {
+        done(null, user._id);
+    });
+    passport.deserializeUser((id, done) => {
+        userModel.findUserById(id)
+            .then(user => {
+                return done(null, user)
+            })
+            .catch(error => {
+                return done(error, null)
+            })
+    });
+}
+module.exports = initPassportLocal
--- src/controllers/SiteController/homeController.js
+++ src/controllers/SiteController/homeController.js
@@ -1,5 +1,7 @@
 let getHome = (req, res) => {
-    return res.render("site/index")
+    return res.render("site/index", {
+        success: req.flash("success")
+    })
 }
 module.exports = {
     getHome: getHome
