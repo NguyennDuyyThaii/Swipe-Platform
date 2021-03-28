c8316056287f6b58f7e7b21e4c66c56598a6bcd3
Word Under Pressure DuyThai <wordunderpressureduythai@Words-MacBook-Pro.local>
2021-03-29 00:40:00 +0700

    logout

--- src/routes/web.js
+++ src/routes/web.js
@@ -18,20 +18,21 @@ let initRouter = (app) => {
     /**
      * Login-register------------------
      */
-    router.get('/login', login.getLogin);
-    router.get('/register', register.getRegister);
-    router.post('/register', userRegister.userRegister, register.postRegister);
-    router.get('/verify/:token', register.verify)
-    router.post('/login', passport.authenticate("local", {
-            successRedirect: "/",
-            failureRedirect: "/login",
-            successFlash: true,
-            failureFlash: true
-        }))
+    router.get('/login', home.checkLoggedOut, login.getLogin);
+    router.get('/register', home.checkLoggedOut, register.getRegister);
+    router.post('/register', home.checkLoggedOut, userRegister.userRegister, register.postRegister);
+    router.get('/verify/:token', home.checkLoggedOut, register.verify)
+    router.post('/login', home.checkLoggedOut, passport.authenticate("local", {
+        successRedirect: "/",
+        failureRedirect: "/login",
+        successFlash: true,
+        failureFlash: true
+    }))
+    router.get('/logout', home.checkloggedIn, home.getLogout)
         /**
          * Home------------------
          */
-    router.get('/', home.getHome)
+    router.get('/', home.checkloggedIn, home.getHome)
 
 
     /**
